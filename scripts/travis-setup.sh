7670bb6e56a8220cbbed024f1743b15bb1d449db
jagrati15 <59571214+jagrati15@users.noreply.github.com>
2020-10-14 12:44:50 +0530

    Update travis-setup.sh

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -31,5 +31,6 @@ else
   curl -LO https://storage.googleapis.com/container-diff/latest/container-diff-linux-amd64 && chmod +x container-diff-linux-amd64 && sudo mv container-diff-linux-amd64 /usr/local/bin/container-diff
   docker run -d -p 5000:5000 --restart always --name registry registry:2
 fi
+
 mkdir -p $HOME/.docker/
 echo '{}' > $HOME/.docker/config.json
